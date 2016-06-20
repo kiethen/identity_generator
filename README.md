3a9b2e984f70f36fbec347b6af1dc99108e34e6f
phinexdaz <wangzheng@soumatoAir.lan>
2016-06-20 21:17:39 +0800

    init

---
+++ README.md
@@ -0,0 +1,55 @@
+# IdentityCard
+
+A chinese identity card generator.
+
+
+
+## Usage
+
+### Command
+
+```
+Usage: IdentityCard.py [options] arg1 arg2
+
+Options:
+
+  --version   show program's version number and exit
+
+  -h, --help  show this help message and exit
+
+  --num=NUM   Number of idcardnumber [default: 1]
+
+  --min=MIN   Minimum age [default: 0]
+
+  --max=MAX   Maximum age [default: 100]
+```
+
+
+
+### API
+
+http://127.0.0.1:5000/api/identidycard
+
+http://127.0.0.1:5000/api/identidycard?num=2
+
+http://127.0.0.1:5000/api/identidycard?num=5&min=20
+
+http://127.0.0.1:5000/api/identidycard?num=10&min=20&max=40
+
+http://127.0.0.1:5000/api/identidycard?num=10&min=20&max=40&sex=1
+
+
+
+### WEB
+
+http://127.0.0.1:5000/?num=2
+
+http://127.0.0.1:5000/?num=5&min=20
+
+http://127.0.0.1:5000/?num=10&min=20&max=40
+
+http://127.0.0.1:5000/?num=10&min=20&max=40&sex=1
+
+
+
+
---
+++ src/web.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+import sys
+import json
+from flask import Flask, request, Response, render_template
+from gevent.pywsgi import WSGIServer
+from gevent import monkey
+from IdentityCard import IdentityCard
+
+default_encoding = 'utf-8'
+if sys.getdefaultencoding() != default_encoding:
+    reload(sys)
+    sys.setdefaultencoding(default_encoding)
+
+monkey.patch_all()
+
+app = Flask(__name__)
+
+
+@app.route('/api/identidycard', methods=['GET'])
+def api_identidycard():
+    num = int(request.args.get('num', '1'))
+    min = int(request.args.get('min', '0'))
+    max = int(request.args.get('max', '100'))
+    sex = int(request.args.get('sex', '0'))
+    cls = IdentityCard(num, min, max, sex)
+    ret = cls.generator()
+    tmp = []
+    for r in ret:
+        tmp.append({'name': r[0], 'id': r[1], 'birthday': r[2], 'age': r[3], 'sex': r[4], 'address': r[5]})
+
+    return Response(
+        response=json.dumps(tmp, ensure_ascii=False, indent=2),
+        status=200,
+        mimetype="application/json;charset=utf-8")
+
+
+@app.route('/', methods=['GET'])
+def web_identidycard():
+    num = int(request.args.get('num', '1'))
+    min = int(request.args.get('min', '0'))
+    max = int(request.args.get('max', '100'))
+    sex = int(request.args.get('sex', '0'))
+    cls = IdentityCard(num, min, max, sex)
+    ret = cls.generator()
+    users = []
+    for r in ret:
+        users.append({'name': r[0], 'id': r[1], 'birthday': r[2], 'age': r[3], 'sex': r[4], 'address': r[5]})
+
+    return render_template('index.html', users=users)
+
+if __name__ == '__main__':
+    http = WSGIServer(('', 5000), app.wsgi_app)
+    http.serve_forever()
